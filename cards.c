7026b22a2979297a0f2adb73484e61c3570f045c
mar.felizardo <mar.felizardo@gmail.com>
2018-07-19 20:15:01 -0300

    message

--- cards.c
+++ cards.c
@@ -3,32 +3,40 @@
 
 int main(){
 	char card_name[3];
-	puts("Enter the card name: ");
-	scanf("%2s", card_name);
-	int val = 0;
-	if (card_name[0] == 'K'){
-		val = 10;
-	}
-	else if (card_name[0] == 'Q'){
-		val = 10;
-	}
-	else if (card_name[0] == 'J'){
-		val = 10;
-	}
-	else if (card_name[0] == 'A'){
-		val = 11;
-	}
-	else {
-		val = atoi(card_name);
-	}
-	if ((val > 2) && (val < 7)){
-		puts("count has gone up...");
-	}
-	else if (val == 10) {
-		puts("count has gone down");
-	}
+	int count = 0;
+	while(card_name[0] != 'x') {
+		puts("Enter the card name: ");
+		scanf("%2s", card_name);
+		int val = 0;
+		
+		switch(card_name[0]) {
+			case 'K':
+			case 'Q':
+			case 'J':
+				val = 10;
+				break;
+			case 'A':
+				val = 11;
+				break;
+			case 'x':
+				continue;
+			default:
+				val = atoi(card_name);
+				if ((val < 1) || (val > 10)){
+					puts("I don't understand the value!");
+					continue;
+				}
 
-	printf("The card value is: %i\n", val);
+		}	
+
+		if ((val > 2) && (val < 7)){
+			count++;
+		}
+		else if (val == 10) {
+			count--;
+		}
+		printf("Current count: %i\n", count);
+	}
 
 	return 0;
 }
